e035343d0fff16c884bf427abe87cc161be480a2
suhan <fndlgosem@naver.com>
2023-02-27 16:48:48 +0900

    test2

--- management/src/App.js
+++ management/src/App.js
@@ -1,14 +1,11 @@
 import React, { Component} from 'react';
-// import logo from './logo.svg';
 import './App.css';
+import Customer from './component/Customer';
 
 class App extends Component {
   render(){
     return (
-      <div className="gray-background">
-      {/* <img src={logo} let="logo"/> */}
-      <h2>let's develop management system</h2>
-    </div>
+      <Customer/>
     );
   }
 }
---
+++ management/src/component/Customer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Customer extends React.Component{
+    render(){
+        return(
+            <div>
+            <p>홍길동</p>
+            <p>생일</p>
+            <p>남자</p>
+            <p>직업</p>
+            </div>                
+        )
+    }
+}
+
+
+export default Customer;
